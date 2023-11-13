0bec023751ba9567df8cc9039139f1973b7ccd4a
Angel Daniel Sanchez Garcia <angel-daniel.sanchez-garcia@epitech.edu>
2023-11-13 13:10:11 +0100

    FEAT: initial basic code

---
+++ vectors.py
@@ -0,0 +1,12 @@
+##
+## EPITECH PROJECT, 2023
+## vectors.py
+## File description:
+## manage vectors
+##
+
+class Vector:
+    def __init__(self, x = 0, y = 0, z = 0):
+        self.x = x
+        self.y = y
+        self.z = z
